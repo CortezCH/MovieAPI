9fdca9943ced9a2cf89a713e1c2737525c65293b
Cortez Christian <CortezChristian@rocketmortgage.com>
2022-01-05 09:03:32 -0500

    Added all code

---
+++ MovieAPI/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        MovieDAL MovieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Results(string title)
+        {
+            SearchParent r = new SearchParent();
+            r = MovieDAL.SearchMoviesByType(title);
+            return View(r);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieAPI/Models/MovieDAL.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MovieAPI.Models
+{
+    public class MovieDAL
+    {
+        public SearchParent SearchMoviesByType(string title)
+        {
+            string url = $"http://www.omdbapi.com/?apikey={Secret.movieKey}&s={title}";
+
+            // Next: Create our request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            // Next: If your API needs any kind of login or key,
+            // that may go here.
+            // SWAPI doesn't need anything special
+
+            // Now we are ready to send off our request and grab the servers
+            // response. Inside out response, the resulting data lives.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Pull result into a stream reader which will then give us a string.
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            // Grab out response string
+            // ReadToEnd starts at the top of our file and returns each line
+            // until it hits the end.
+            string result = rd.ReadToEnd();
+            rd.Close();
+            // This line converts out Json string
+            // into a person object automatically
+            SearchParent p = JsonConvert.DeserializeObject<SearchParent>(result);
+
+            // Later we'll convert our string into a model which makes it much easier to 
+            // use for .net
+            return p;
+        }
+    }
+}
